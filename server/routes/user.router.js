3d1cdbaa643bd2322630f0e212c63a1b2bd1287f
Pete St. Martin <47460632+petestmart@users.noreply.github.com>
2023-04-24 22:48:04 -0500

    Dependency Updates

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -17,7 +17,9 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {  
   const username = req.body.username;
+  console.log("user.router - username: " + username)
   const password = encryptLib.encryptPassword(req.body.password);
+  console.log("user.router - password: " + password)
 
   const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
   pool.query(queryText, [username, password])
